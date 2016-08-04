105fe6fe07f7c0ef47ff45a37781b7b1780e33a4
yoyo <18829290140@163.com>
2016-08-04 21:04:27 +0800

    demo2

--- program.js
+++ program.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.argv[2] || 3000));
+app.set('port', (process.argv[2] || 3001));
 app.set('view engine', 'jsx');
 app.set('views', __dirname + '/views');
 app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
